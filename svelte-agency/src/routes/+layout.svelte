9b4ff91dd04eb7e86e41209c81d984402465a22a
vmsirish <sirish@hatrio.ai>
2026-07-30 18:46:48 +0530

    Verified fixes for all templates

--- svelte-agency/src/routes/+layout.svelte
+++ svelte-agency/src/routes/+layout.svelte
@@ -1,11 +1,4 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+<script>
+  import '../app.css';
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children()}
+<slot />
